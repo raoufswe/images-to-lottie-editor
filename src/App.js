590b3d96708c3f0a11fe34969d5c1f5b3aea193b
Maadh <maadh@protonmail.ch>
2021-03-03 18:43:12 +0500

    New lottie

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import { useQuery } from "react-query"
-import axios from "axios"
-import { Box, Flex, Grid, Text, Heading } from "@chakra-ui/react"
-import { Player, Controls } from "@lottiefiles/react-lottie-player"
-import testLottie from "./test.json"
+import { useQuery } from "react-query";
+import axios from "axios";
+import { Box, Flex, Grid, Text, Heading } from "@chakra-ui/react";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import testLottie from "./among-us.json";
 
-// const url = `https://assets10.lottiefiles.com/private_files/lf30_k5obblw6.json`
+// const url = `https://assets10.lottiefiles.com/private_files/lf30_k5obblw6.json`;
+// const url = `https://assets4.lottiefiles.com/packages/lf20_x6wwkwp2.json`;
 // const colorsAPI = "https://editor-api.lottiefiles.com/colors"
 
 function App() {
   // const { data } = useQuery(url, async () => {
-  //   const { data } = await axios.get(url)
-  //   return data
-  // })
+  //   const { data } = await axios.get(url);
+  //   return data;
+  // });
   // const colorQuery = useQuery([colorsAPI, url, "colors"], async () => {
   //   const { data } = await axios.post(colorsAPI, { url, type: "shape", color: "" })
   //   return data
   // })
-  console.log(testLottie)
+  console.log(testLottie);
 
   return (
     <Grid gridTemplateColumns="1fr 2fr 1fr" h="100vh" w="100vw">
-      <Flex flexDir="column" m="4" overflow="auto" height="90%" borderRight="2px">
+      <Flex
+        flexDir="column"
+        m="4"
+        overflow="auto"
+        height="90%"
+        borderRight="2px"
+      >
         <Heading size="md" mb="4">
           Layers
         </Heading>
@@ -30,7 +37,7 @@ function App() {
               <Player
                 src={{
                   ...testLottie,
-                  layers: testLottie.layers.filter((i) => i.nm === layer.nm)
+                  layers: testLottie.layers.filter((i) => i.nm === layer.nm),
                 }}
               />
             </Box>
@@ -41,12 +48,15 @@ function App() {
       </Flex>
       <Flex margin="auto 4em">
         <Player autoplay loop src={testLottie}>
-          <Controls visible={true} buttons={["play", "repeat", "frame", "debug"]} />
+          <Controls
+            visible={true}
+            buttons={["play", "repeat", "frame", "debug"]}
+          />
         </Player>
       </Flex>
       <div>Configs</div>
     </Grid>
-  )
+  );
 }
 
-export default App
+export default App;
