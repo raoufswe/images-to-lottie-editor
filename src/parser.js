ef1c9f3e26f097bd582772567ac19fd1e40d9a37
Raouf <abdulraoufswehli58@gmail.com>
2021-03-04 03:34:59 +0800

    added configs

--- src/parser.js
+++ src/parser.js
@@ -26,18 +26,6 @@ export const composeNewLayer = (lottieFile, newImage) => {
                 t: 0,
                 s: [100, 100, 0]
               },
-              {
-                i: { x: [0.667, 0.667, 0.667], y: [1, 1, 1] },
-                o: { x: [0.333, 0.333, 0.333], y: [0, 0, 0] },
-                t: 15,
-                s: [100, 50, 100]
-              },
-              {
-                i: { x: [0.667, 0.667, 0.667], y: [1, 1, 1] },
-                o: { x: [0.333, 0.333, 0.333], y: [0, 0, 0] },
-                t: 30,
-                s: [100, 100, 0]
-              },
               { t: 59.0000024031193, s: [100, 100, 100] }
             ],
             ix: 6
@@ -51,4 +39,46 @@ export const composeNewLayer = (lottieFile, newImage) => {
       }
     ]
   }
+}
+
+export const deleteLayer = (lottieFile, selected_layer) => {
+  let cloned = JSON.parse(JSON.stringify(lottieFile))
+  return {
+    ...cloned,
+    ...(cloned?.assets ? { assets: cloned.assets.filter((asset) => asset.id !== selected_layer.nm) } : {}),
+    layers: cloned.layers.filter((layer) => layer.nm !== selected_layer.nm)
+  }
+}
+
+export const updateOpacity = (lottieFile, selected_layer, opacity) => {
+  let cloned = JSON.parse(JSON.stringify(lottieFile))
+  return {
+    ...cloned,
+    layers: [...cloned.layers].map((layer) => {
+      if (layer.nm === selected_layer?.nm) layer.ks.o.k = opacity
+      return layer
+    })
+  }
+}
+
+export const resizeAsset = (lottieFile, selected_layer, { h, w }) => {
+  let cloned = JSON.parse(JSON.stringify(lottieFile))
+  return {
+    ...cloned,
+    ...(cloned?.assets
+      ? {
+          assets: [...cloned.assets].map((asset) => {
+            if (asset.id === selected_layer.nm) {
+              asset.h = h ? h : asset.h
+              asset.w = w ? w : asset.w
+            }
+            return asset
+          })
+        }
+      : {})
+  }
+}
+
+export const getAsset = (lottieFile, id) => {
+  return lottieFile.assets.find((asset) => asset.id === id)
 }
