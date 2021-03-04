a81cafa2a7f0c7d3bdd8d045138142d851dd4dc3
Maadh <maadh@protonmail.ch>
2021-03-04 20:24:26 +0500

    FIX: Added position x and y

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from "react";
 import {
   Box,
   Flex,
@@ -14,67 +14,99 @@ import {
   NumberInputField,
   NumberInputStepper,
   NumberIncrementStepper,
-  NumberDecrementStepper
-} from "@chakra-ui/react"
-import { Player, Controls } from "@lottiefiles/react-lottie-player"
-import testLottie from "./among-us.json"
-import FileBase64 from "react-file-base64"
-import { composeNewLayer, deleteLayer, updateOpacity, resizeAsset, getAsset } from "./parser"
-import { CloseIcon } from "@chakra-ui/icons"
+  NumberDecrementStepper,
+} from "@chakra-ui/react";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import testLottie from "./among-us.json";
+import FileBase64 from "react-file-base64";
+import {
+  composeNewLayer,
+  deleteLayer,
+  updateOpacity,
+  resizeAsset,
+  getAsset,
+  moveAsset,
+} from "./parser";
+import { CloseIcon } from "@chakra-ui/icons";
 
 function App() {
-  const [lottieFile, setLottieFile] = useState(testLottie)
-  const [selectedLayer, setSelectedLayer] = useState(null)
-  const [configs, setConfigs] = useState()
+  const [lottieFile, setLottieFile] = useState(testLottie);
+  const [selectedLayer, setSelectedLayer] = useState(null);
+  const [configs, setConfigs] = useState();
 
   const onImageUploaded = (newImage) => {
-    const newLottie = composeNewLayer(lottieFile, newImage)
-    setLottieFile(newLottie)
-  }
+    const newLottie = composeNewLayer(lottieFile, newImage);
+    setLottieFile(newLottie);
+  };
 
   const onLayerDelete = (layer) => {
-    const newLottieFile = deleteLayer(lottieFile, layer)
-    setLottieFile(newLottieFile)
-  }
+    const newLottieFile = deleteLayer(lottieFile, layer);
+    setLottieFile(newLottieFile);
+  };
 
   const onOpacityChange = (value) => {
-    const newLottieFile = updateOpacity(lottieFile, selectedLayer, value)
-    setLottieFile(newLottieFile)
-  }
+    const newLottieFile = updateOpacity(lottieFile, selectedLayer, value);
+    setLottieFile(newLottieFile);
+  };
 
   const onResizeAsset = ({ h, w }) => {
-    const newLottieFile = resizeAsset(lottieFile, selectedLayer, { h, w })
-    setLottieFile(newLottieFile)
-  }
+    const newLottieFile = resizeAsset(lottieFile, selectedLayer, { h, w });
+    setLottieFile(newLottieFile);
+  };
 
-  const selectedAsset = getAsset(lottieFile, selectedLayer?.nm)
+  const onMoveAsset = ({ x, y }) => {
+    const newLottieFile = moveAsset(lottieFile, selectedLayer, { x, y });
+    setLottieFile(newLottieFile);
+  };
+
+  const selectedAsset = getAsset(lottieFile, selectedLayer?.nm);
 
   const onLayerSelect = (layer) => {
-    setSelectedLayer(layer)
-    const selectedAsset = getAsset(lottieFile, layer?.nm)
-    setConfigs({ ...configs, ...(selectedAsset ? { w: selectedAsset.w, h: selectedAsset.h } : {}), opacity: layer.ks.o.k })
-  }
+    setSelectedLayer(layer);
+    const selectedAsset = getAsset(lottieFile, layer?.nm);
+    setConfigs({
+      ...configs,
+      ...(selectedAsset ? { w: selectedAsset.w, h: selectedAsset.h } : {}),
+      ...(selectedLayer
+        ? { x: selectedLayer.ks.a.k[0], y: selectedLayer.ks.a.k[1] }
+        : {}),
+      opacity: layer.ks.o.k,
+    });
+  };
 
   const handleChange = (value, name) => {
-    setConfigs({ ...configs, [name]: value })
+    setConfigs({ ...configs, [name]: value });
     switch (name) {
       case "opacity":
-        onOpacityChange(value)
-        break
+        onOpacityChange(value);
+        break;
       case "w":
-        onResizeAsset({ w: value })
-        break
+        onResizeAsset({ w: value });
+        break;
       case "h":
-        onResizeAsset({ h: value })
-        break
+        onResizeAsset({ h: value });
+        break;
+      case "x":
+        onMoveAsset({ x: value });
+        break;
+      case "y":
+        onMoveAsset({ y: value });
+        break;
       default:
-        break
+        break;
     }
-  }
+  };
 
   return (
     <Grid gridTemplateColumns="1fr 2fr 1fr">
-      <Flex flexDir="column" m="1rem" overflow="auto" borderRight="2px" w="100%" h="100vh">
+      <Flex
+        flexDir="column"
+        m="1rem"
+        overflow="auto"
+        borderRight="2px"
+        w="100%"
+        h="100vh"
+      >
         <Flex mb="4" flexDir="column">
           <Heading mb="4" size="md">
             Upload your images
@@ -85,20 +117,42 @@ function App() {
           Layers
         </Heading>
         {lottieFile.layers.map((layer) => (
-          <Flex mb="4" alignItems="center" key={layer.nm} my="2" mr="4" bg={selectedLayer?.nm === layer.nm ? "teal" : ""} borderRadius="4px" p="2">
+          <Flex
+            mb="4"
+            alignItems="center"
+            key={layer.nm}
+            my="2"
+            mr="4"
+            bg={selectedLayer?.nm === layer.nm ? "teal" : ""}
+            borderRadius="4px"
+            p="2"
+          >
             <Flex onClick={onLayerSelect.bind(null, layer)}>
               <Box w="50px" h="50x" borderRadius="4px " bg="white">
-                <Player src={{ ...lottieFile, layers: lottieFile.layers.filter((i) => i.nm === layer.nm) }} />
+                <Player
+                  src={{
+                    ...lottieFile,
+                    layers: lottieFile.layers.filter((i) => i.nm === layer.nm),
+                  }}
+                />
               </Box>
               <Text ml="2">{layer.nm}</Text>
             </Flex>
-            <IconButton icon={<CloseIcon />} onClick={onLayerDelete.bind(null, layer)} size="sm" ml="auto" />
+            <IconButton
+              icon={<CloseIcon />}
+              onClick={onLayerDelete.bind(null, layer)}
+              size="sm"
+              ml="auto"
+            />
           </Flex>
         ))}
       </Flex>
       <Flex margin="auto 4em" flexDir="column">
         <Player autoplay loop src={lottieFile}>
-          <Controls visible={true} buttons={["play", "repeat", "frame", "debug"]} />
+          <Controls
+            visible={true}
+            buttons={["play", "repeat", "frame", "debug"]}
+          />
         </Player>
       </Flex>
       <Box p="2rem">
@@ -127,7 +181,13 @@ function App() {
                   <Heading size="sm" mb="2">
                     Width
                   </Heading>
-                  <NumberInput name="w" inputMode="numeric" defaultValue={selectedAsset.w} onChange={(value) => handleChange(value, "w")} value={configs.w}>
+                  <NumberInput
+                    name="w"
+                    inputMode="numeric"
+                    defaultValue={selectedAsset.w}
+                    onChange={(value) => handleChange(value, "w")}
+                    value={configs.w}
+                  >
                     <NumberInputField />
                     <NumberInputStepper>
                       <NumberIncrementStepper />
@@ -140,7 +200,51 @@ function App() {
                   <Heading size="sm" mb="2">
                     Height
                   </Heading>
-                  <NumberInput name="h" inputMode="numeric" defaultValue={selectedAsset.h} onChange={(value) => handleChange(value, "h")} value={configs.h}>
+                  <NumberInput
+                    name="h"
+                    inputMode="numeric"
+                    defaultValue={selectedAsset.h}
+                    onChange={(value) => handleChange(value, "h")}
+                    value={configs.h}
+                  >
+                    <NumberInputField />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </Flex>
+
+                <Flex flexDir="column" w="100%" mt="2">
+                  <Heading size="sm" mb="2">
+                    Position X
+                  </Heading>
+                  <NumberInput
+                    name="x"
+                    inputMode="numeric"
+                    defaultValue={selectedLayer.ks.a.k[0]}
+                    onChange={(value) => handleChange(value, "x")}
+                    value={configs.x}
+                  >
+                    <NumberInputField />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </Flex>
+
+                <Flex flexDir="column" w="100%" mt="2">
+                  <Heading size="sm" mb="2">
+                    Position Y
+                  </Heading>
+                  <NumberInput
+                    name="y"
+                    inputMode="numeric"
+                    defaultValue={selectedLayer.ks.a.k[1]}
+                    onChange={(value) => handleChange(value, "y")}
+                    value={configs.y}
+                  >
                     <NumberInputField />
                     <NumberInputStepper>
                       <NumberIncrementStepper />
@@ -154,7 +258,7 @@ function App() {
         )}
       </Box>
     </Grid>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/parser.js
+++ src/parser.js
@@ -1,8 +1,11 @@
 export const composeNewLayer = (lottieFile, newImage) => {
-  let cloned = JSON.parse(JSON.stringify(lottieFile))
+  let cloned = JSON.parse(JSON.stringify(lottieFile));
   return {
     ...cloned,
-    assets: [...cloned.assets, { id: newImage.name, w: 500, h: 500, u: "", p: newImage.base64, e: 1 }],
+    assets: [
+      ...cloned.assets,
+      { id: newImage.name, w: 500, h: 500, u: "", p: newImage.base64, e: 1 },
+    ],
     layers: [
       ...cloned.layers,
       {
@@ -24,61 +27,87 @@ export const composeNewLayer = (lottieFile, newImage) => {
                 i: { x: [0.667, 0.667, 0.667], y: [1, 1, 1] },
                 o: { x: [0.333, 0.333, 0.333], y: [0, 0, 0] },
                 t: 0,
-                s: [100, 100, 0]
+                s: [100, 100, 0],
               },
-              { t: 59.0000024031193, s: [100, 100, 100] }
+              { t: 59.0000024031193, s: [100, 100, 100] },
             ],
-            ix: 6
-          }
+            ix: 6,
+          },
         },
         ao: 0,
         ip: 0,
         op: 60.0000024438501,
         st: 0,
-        bm: 0
-      }
-    ]
-  }
-}
+        bm: 0,
+      },
+    ],
+  };
+};
 
 export const deleteLayer = (lottieFile, selected_layer) => {
-  let cloned = JSON.parse(JSON.stringify(lottieFile))
+  let cloned = JSON.parse(JSON.stringify(lottieFile));
   return {
     ...cloned,
-    ...(cloned?.assets ? { assets: cloned.assets.filter((asset) => asset.id !== selected_layer.nm) } : {}),
-    layers: cloned.layers.filter((layer) => layer.nm !== selected_layer.nm)
-  }
-}
+    ...(cloned?.assets
+      ? {
+          assets: cloned.assets.filter(
+            (asset) => asset.id !== selected_layer.nm
+          ),
+        }
+      : {}),
+    layers: cloned.layers.filter((layer) => layer.nm !== selected_layer.nm),
+  };
+};
 
 export const updateOpacity = (lottieFile, selected_layer, opacity) => {
-  let cloned = JSON.parse(JSON.stringify(lottieFile))
+  let cloned = JSON.parse(JSON.stringify(lottieFile));
   return {
     ...cloned,
     layers: [...cloned.layers].map((layer) => {
-      if (layer.nm === selected_layer?.nm) layer.ks.o.k = opacity
-      return layer
-    })
-  }
-}
+      if (layer.nm === selected_layer?.nm) layer.ks.o.k = opacity;
+      return layer;
+    }),
+  };
+};
 
 export const resizeAsset = (lottieFile, selected_layer, { h, w }) => {
-  let cloned = JSON.parse(JSON.stringify(lottieFile))
+  let cloned = JSON.parse(JSON.stringify(lottieFile));
   return {
     ...cloned,
     ...(cloned?.assets
       ? {
           assets: [...cloned.assets].map((asset) => {
             if (asset.id === selected_layer.nm) {
-              asset.h = h ? h : asset.h
-              asset.w = w ? w : asset.w
+              asset.h = h ? h : asset.h;
+              asset.w = w ? w : asset.w;
             }
-            return asset
-          })
+            return asset;
+          }),
         }
-      : {})
-  }
-}
+      : {}),
+  };
+};
+
+export const moveAsset = (lottieFile, selected_layer, { x, y }) => {
+  let cloned = JSON.parse(JSON.stringify(lottieFile));
+  return {
+    ...cloned,
+    layers: [...cloned.layers].map((layer) => {
+      if (layer.nm === selected_layer?.nm) {
+        layer.ks.a = {
+          ...layer.ks.a,
+          k: [
+            x ? parseInt(x) : selected_layer.ks.a.k[0],
+            y ? parseInt(y) : selected_layer.ks.a.k[1],
+            0,
+          ],
+        };
+      }
+      return layer;
+    }),
+  };
+};
 
 export const getAsset = (lottieFile, id) => {
-  return lottieFile.assets.find((asset) => asset.id === id)
-}
+  return lottieFile.assets.find((asset) => asset.id === id);
+};
