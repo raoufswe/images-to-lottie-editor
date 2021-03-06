45f547ec26c51e5436f26a14d89d40e5a9f064e5
Maadh <maadh@protonmail.ch>
2021-03-06 17:24:43 +0500

    FIX: Minor changes to animation

--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,21 @@
 export const composeNewLayer = (lottieFile, { image, externalBase64 = "" }) => {
-  let cloned = JSON.parse(JSON.stringify(lottieFile))
-  const refId = `refId-${image.name}-${new Date()}`
+  let cloned = JSON.parse(JSON.stringify(lottieFile));
+  const refId = `refId-${image.name}-${new Date()}`;
   return {
     ...cloned,
-    assets: [{ id: refId, w: 500, h: 500, u: "", p: image?.base64 ?? externalBase64, e: 1 }, ...cloned.assets],
+    assets: [
+      ...cloned.assets,
+      {
+        id: refId,
+        w: 500,
+        h: 500,
+        u: "",
+        p: image?.base64 ?? externalBase64,
+        e: 1,
+      },
+    ],
     layers: [
+      ...cloned.layers,
       {
         ddd: 0,
         ind: 1,
@@ -24,23 +35,22 @@ export const composeNewLayer = (lottieFile, { image, externalBase64 = "" }) => {
                 i: { x: [0.667, 0.667, 0.667], y: [1, 1, 1] },
                 o: { x: [0.333, 0.333, 0.333], y: [0, 0, 0] },
                 t: 0,
-                s: [100, 100, 0]
+                s: [100, 100, 0],
               },
-              { t: 59.0000024031193, s: [100, 100, 100] }
+              { t: 59.0000024031193, s: [100, 100, 100] },
             ],
-            ix: 6
-          }
+            ix: 6,
+          },
         },
         ao: 0,
         ip: 0,
         op: 60.0000024438501,
         st: 0,
-        bm: 0
+        bm: 0,
       },
-      ...cloned.layers
-    ]
-  }
-}
+    ],
+  };
+};
 
 const bounceAnimation = {
   o: { a: 0, k: 100, ix: 11 },
@@ -54,25 +64,25 @@ const bounceAnimation = {
         i: { x: [0.667, 0.667, 0.667], y: [1, 1, 1] },
         o: { x: [0.333, 0.333, 0.333], y: [0, 0, 0] },
         t: 0,
-        s: [100, 100, 100]
+        s: [100, 100, 100],
       },
       {
         i: { x: [0.667, 0.667, 0.667], y: [1, 1, 1] },
         o: { x: [0.333, 0.333, 0.333], y: [0, 0, 0] },
         t: 13.5,
-        s: [100, 50, 100]
+        s: [100, 50, 100],
       },
       {
         i: { x: [0.667, 0.667, 0.667], y: [1, 1, 1] },
         o: { x: [0.333, 0.333, 0.333], y: [0, 0, 0] },
         t: 27,
-        s: [100, 100, 100]
+        s: [100, 100, 100],
       },
-      { t: 59.0000024031193, s: [100, 100, 100] }
+      { t: 59.0000024031193, s: [100, 100, 100] },
     ],
-    ix: 6
-  }
-}
+    ix: 6,
+  },
+};
 
 const appearAnimation = {
   ty: "tr",
@@ -87,27 +97,27 @@ const appearAnimation = {
         i: { x: [0.67, 0.67, 0.67], y: [1, 1, 1] },
         o: { x: [0.33, 0.33, 0.33], y: [0, 0, 0] },
         t: 0,
-        s: [0, 0, 100]
+        s: [0, 0, 100],
       },
       {
         i: { x: [0.67, 0.67, 0.67], y: [1, 1, 1] },
         o: { x: [0.33, 0.33, 0.33], y: [0, 0, 0] },
         t: 34,
-        s: [135, 135, 100]
+        s: [135, 135, 100],
       },
       {
         i: { x: [0.83, 0.83, 0.83], y: [1, 1, 1] },
         o: { x: [0.33, 0.33, 0.33], y: [0, 0, 0] },
         t: 45,
-        s: [131, 131, 100]
+        s: [131, 131, 100],
       },
-      { t: 50, s: [133, 133, 100] }
+      { t: 50, s: [133, 133, 100] },
     ],
-    ix: 6
+    ix: 6,
   },
   sk: { k: 0 },
-  sa: { k: 0 }
-}
+  sa: { k: 0 },
+};
 
 const rotateAnimation = {
   ty: "tr",
@@ -121,7 +131,7 @@ const rotateAnimation = {
         t: 0,
         s: [0, -512, 0],
         to: [0, -76.58, 0],
-        ti: [0, 2.38, 0]
+        ti: [0, 2.38, 0],
       },
       {
         i: { x: 0.67, y: 1 },
@@ -129,89 +139,102 @@ const rotateAnimation = {
         t: 37,
         s: [0, 384, 0],
         to: [0, -6.63, 0],
-        ti: [0, -0.48, 0]
+        ti: [0, -0.48, 0],
       },
-      { t: 26, s: [0, 0, 0] }
+      { t: 26, s: [0, 0, 0] },
     ],
     ix: 2,
-    a: 1
+    a: 1,
   },
   a: { k: [0, 0] },
-  s: { k: [133.33, 133.33] },
+  s: { k: [100, 100] },
   sk: { k: 0 },
-  sa: { k: 0 }
-}
+  sa: { k: 0 },
+};
 
 export const animations = [
   { name: "Bounce", value: bounceAnimation },
   { name: "Appear", value: appearAnimation },
-  { name: "Rotate", value: rotateAnimation }
-]
+  { name: "Drop down", value: rotateAnimation },
+  { name: "Default", value: {} },
+];
 
 export const deleteLayer = (lottieFile, selected_layer) => {
-  let cloned = JSON.parse(JSON.stringify(lottieFile))
-  if (cloned?.assets) cloned.assets = cloned.assets.filter((asset) => asset.id !== selected_layer.refId)
-  cloned.layers = cloned.layers.filter((layer) => layer.refId !== selected_layer.refId)
-  return cloned
-}
+  let cloned = JSON.parse(JSON.stringify(lottieFile));
+  if (cloned?.assets)
+    cloned.assets = cloned.assets.filter(
+      (asset) => asset.id !== selected_layer.refId
+    );
+  cloned.layers = cloned.layers.filter(
+    (layer) => layer.refId !== selected_layer.refId
+  );
+  return cloned;
+};
 
 export const updateOpacity = (lottieFile, selected_layer, opacity) => {
-  let cloned = JSON.parse(JSON.stringify(lottieFile))
+  let cloned = JSON.parse(JSON.stringify(lottieFile));
   cloned.layers = [...cloned.layers].map((layer) => {
-    if (layer.refId === selected_layer?.refId) layer.ks.o.k = opacity
-    return layer
-  })
-  return cloned
-}
+    if (layer.refId === selected_layer?.refId) layer.ks.o.k = opacity;
+    return layer;
+  });
+  return cloned;
+};
 
 export const resizeAsset = (lottieFile, selected_layer, { h, w }) => {
-  let cloned = JSON.parse(JSON.stringify(lottieFile))
+  let cloned = JSON.parse(JSON.stringify(lottieFile));
   if (cloned?.assets) {
     cloned.assets = [...cloned.assets].map((asset) => {
       if (asset.id === selected_layer.refId) {
-        asset.h = h ? h : asset.h
-        asset.w = w ? w : asset.w
+        asset.h = h ? h : asset.h;
+        asset.w = w ? w : asset.w;
       }
-      return asset
-    })
+      return asset;
+    });
   }
 
-  return cloned
-}
+  return cloned;
+};
 
 export const moveAsset = (lottieFile, selected_layer, { x, y }) => {
-  let cloned = JSON.parse(JSON.stringify(lottieFile))
+  let cloned = JSON.parse(JSON.stringify(lottieFile));
   cloned.layers = [...cloned.layers].map((layer) => {
     if (layer.refId === selected_layer.refId) {
-      layer.ks.a.k = [x ? parseFloat(x) : parseFloat(layer.ks.a.k[0]), y ? parseFloat(y) : parseFloat(layer.ks.a.k[1]), 0]
+      layer.ks.a = {
+        ...layer.ks.a,
+        k: [
+          x ? parseFloat(x) : parseFloat(layer.ks.a.k[0]),
+          y ? parseFloat(y) : parseFloat(layer.ks.a.k[1]),
+          0,
+        ],
+      };
     }
-    return layer
-  })
+    return layer;
+  });
 
-  return cloned
-}
+  return cloned;
+};
 
 export const getAsset = (lottieFile, id) => {
-  return lottieFile.assets.find((asset) => asset.id === id)
-}
+  return lottieFile.assets.find((asset) => asset.id === id);
+};
 
 export const updateFrameRate = (lottieFile, newFrameRate) => {
-  let cloned = JSON.parse(JSON.stringify(lottieFile))
-  cloned.fr = parseFloat(newFrameRate)
-  return cloned
-}
+  let cloned = JSON.parse(JSON.stringify(lottieFile));
+  cloned.fr = parseFloat(newFrameRate);
+  return cloned;
+};
 
 export const updateAnimation = (lottieFile, selected_layer, index) => {
-  let cloned = JSON.parse(JSON.stringify(lottieFile))
+  let cloned = JSON.parse(JSON.stringify(lottieFile));
 
   cloned.layers = [...cloned.layers].map((layer) => {
     if (layer.refId === selected_layer?.refId) {
-      index === 0 && (animations[index].value.a = layer.ks.a)
-      layer.ks = animations[index].value
-      return layer
+      index === 0 && (animations[index].value.a = layer.ks.a);
+      layer.ks = animations[index].value;
+      return layer;
     }
-    return layer
-  })
+    return layer;
+  });
 
-  return cloned
-}
+  return cloned;
+};
